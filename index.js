9e3ee40a508291a7cd9a4306b72487adcf1ab322
dee288 <dianamajala10@gmail.com>
2022-06-19 22:30:58 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-function isPalindrome(word) {
+
   // Write your algorithm here
-}
+  const str =("Hannah")
+  const isPalindrome =(newStr) => {
+    newStr= newStr.toLowerCase()
+    for (let i = 0; i < newStr.length / 2; i++){
+      if (newStr[i] !== newStr[newStr.length - 1 - i]){
+        return false;
+      }
+    }
+    return true
+  }
+
+console.log(isPalindrome(str))
+
 
 /* 
   Add your pseudocode here
+  When the first letter is the same as last letter,then second letter is the same
+  as the second to last letter, the third letter is the same as the third to the last letter etc. until we reach the middle, it returns true.
+  Iterate from the beginning to the middle as it checks each letter to the correponding letter from the end
+if any of the letters doesnt match, return false
+  
 */
 
 /*
@@ -13,6 +30,12 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('hannah'))
+
+console.log(' ')
+  
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
